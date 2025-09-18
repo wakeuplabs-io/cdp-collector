9333fea186534f667113e5cdf3534a89df54323a
Matias Zapata <matuzapata@gmail.com>
2025-09-17 21:17:45 -0600

    pause btc and weth for swaps

--- apps/www/src/config.ts
+++ apps/www/src/config.ts
@@ -42,22 +42,6 @@ export const NATIVE_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 export const SUPPORTED_ASSETS_BY_CHAIN: Record<Network, Token[]> = {
   base: [
     {
-      decimals: 6,
-      name: "USDC",
-      symbol: "USDC",
-      address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
-      iconUrl:
-        "https://dynamic-assets.coinbase.com/3c15df5e2ac7d4abbe9499ed9335041f00c620f28e8de2f93474a9f432058742cdf4674bd43f309e69778a26969372310135be97eb183d91c492154176d455b8/asset_icons/9d67b728b6c8f457717154b3a35f9ddc702eae7e76c4684ee39302c4d7fd0bb8.png",
-    },
-    {
-      decimals: 8,
-      name: "Coinbase Wrapped Bitcoin",
-      symbol: "cbBTC",
-      address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
-      iconUrl:
-        "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png",
-    },
-    {
       decimals: 18,
       name: "Ethereum",
       symbol: "ETH",
@@ -66,13 +50,29 @@ export const SUPPORTED_ASSETS_BY_CHAIN: Record<Network, Token[]> = {
         "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
     },
     {
-      decimals: 18,
-      name: "Wrapped Ethereum",
-      symbol: "WETH",
-      address: "0x4200000000000000000000000000000000000006",
+      decimals: 6,
+      name: "USDC",
+      symbol: "USDC",
+      address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
       iconUrl:
-        "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
+        "https://dynamic-assets.coinbase.com/3c15df5e2ac7d4abbe9499ed9335041f00c620f28e8de2f93474a9f432058742cdf4674bd43f309e69778a26969372310135be97eb183d91c492154176d455b8/asset_icons/9d67b728b6c8f457717154b3a35f9ddc702eae7e76c4684ee39302c4d7fd0bb8.png",
     },
+    // {
+    //   decimals: 8,
+    //   name: "Coinbase Wrapped Bitcoin",
+    //   symbol: "cbBTC",
+    //   address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
+    //   iconUrl:
+    //     "https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png",
+    // },
+    // {
+    //   decimals: 18,
+    //   name: "Wrapped Ethereum",
+    //   symbol: "WETH",
+    //   address: "0x4200000000000000000000000000000000000006",
+    //   iconUrl:
+    //     "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
+    // },
   ],
   "base-sepolia": [
     {
